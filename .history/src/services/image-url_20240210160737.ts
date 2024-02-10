a7fd65d1df3f7da3d6efa67668836368ba671b92
HAONANTAO <873190199@qq.com>
2024-02-10 16:08:22 +1100

    handle game with no image

--- .history/src/services/image-url_20240210160737.ts
+++ .history/src/services/image-url_20240210160737.ts
@@ -1,7 +1,5 @@
-import noImage from "../assets/no-image-placeholder.webp";
-
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return noImage;
+  if (!url) return "../assets/";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
